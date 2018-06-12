b43880006923455df9713572bf1e0a0144117d8d
lichunli <lichunli@lichunlideMac-mini.local>
2018-06-12 09:17:12 +0800

    bug修复

--- src/components/businessManage/tradeProcess.vue
+++ src/components/businessManage/tradeProcess.vue
@@ -283,6 +283,38 @@
                     </el-row>
                   </div>
                 </div>
+                <div class="detail-list detail-form" v-if="item.type === 'cancel_check_end'">
+                  <div class="process-content">
+                    <el-row :gutter="10">
+                      <el-col :span="8">
+                        <div class="label-list">
+                          <label>取消-部门审核时间:</label>
+                          <div class="detail-form-item" v-html="pbFunc.dealNullData(item.operated_at)"></div>
+                        </div>
+                      </el-col>
+                      <el-col :span="8">
+                        <div class="label-list">
+                          <label>取消-部门审核结果:</label>
+                          <div class="detail-form-item" v-html="pbFunc.dealNullData(item.operation_cn)"></div>
+                        </div>
+                      </el-col>
+                      <el-col :span="8">
+                        <div class="label-list">
+                          <label>操作人:</label>
+                          <div class="detail-form-item" v-html="pbFunc.dealNullData(item.operator)"></div>
+                        </div>
+                      </el-col>
+                    </el-row>
+                    <el-row :gutter="10" v-if="item.operation==='denied'">
+                      <el-col :span="24">
+                        <div class="label-list">
+                          <label>拒绝理由:</label>
+                          <div class="detail-form-item" v-html="pbFunc.dealNullData(item.refuse_note)"></div>
+                        </div>
+                      </el-col>
+                    </el-row>
+                  </div>
+                </div>
                 <div class="detail-list detail-form" v-if="item.type === 'modify_department_check'">
                   <div class="process-content">
                     <el-row :gutter="10">
@@ -398,7 +430,8 @@ export default {
         modify_department_check: '修改审核——部门审核中',
         modify_department_check_end: '修改审核——部门审核完成',
         settlement_check: '结算审核——经理审核中',
-        cancel_check: '取消审核——服务中心审核中',
+        cancel_check: '取消审核——部门审核中',
+        cancel_check_end: '取消审核——部门审核完成',
       },
       processData: {},
       refuseTitle: ''
@@ -419,15 +452,15 @@ export default {
       if (data.length) status = data[0].type;
       console.log('status', status);
       switch (status) {
-        case 'modify_department_check': //部门审核
+        case 'modify_department_check': //部门修改
           return true;
           break;
-        case 'create_department_check': //部门修改
+        case 'create_department_check': //部门审核
           return true;
           break;
-          // case 'plan_tonnage':
-          //   return '计划吨位';
-          //   break;
+          case 'cancel_check'://取消审核
+            return true;
+            break;
           // case 'total_price':
           //   return '销售总价';
           //   break;
@@ -472,9 +505,13 @@ export default {
           for (let i in this.processData) {
             this.processData[i].operation_cn = this.processData[i].operation && this.processData[i].operation === 'pass' ? '通过' : '拒绝';
             if (i>0&&this.processData[i].type === 'modify_department_check') {
-              // console.log('审核',typeof this.processData[i].to_be_modify,this.processData[i])
               if (typeof this.processData[i].to_be_modify != "object") {
                 this.processData[i].type = 'modify_department_check_end'
+              }
+            }
+            if (i>0&&this.processData[i].type === 'cancel_check') {
+              if (typeof this.processData[i].to_be_modify != "object") {
+                this.processData[i].type = 'cancel_check_end'
               }
             }
             for (let j in this.statusType) {
@@ -508,7 +545,7 @@ export default {
       }else if (status === 'modify_department_check') {
         title = '业务单取修改核通过';
         desc = '修改审核';
-      } else if (status === 'waiting_confirm') {
+      } else if (status === 'cancel_check') {
         title = '业务单取消审核通过';
         desc = '通过后，业务单取消，卸货计划将被自动取消';
       }
