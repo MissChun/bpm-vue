9b9910b077abfd7a0976897b6ada9b406fbca550
tianxiaolong <13568960272@163.com>
2018-07-15 20:23:46 +0800

    修改线上客户管理bug

--- src/api/index.js
+++ src/api/index.js
@@ -27,7 +27,7 @@ if (currentUrl.match('pbpm.91lng.cn')) {
   domainUrl = 'http://bpm.91lng.cn';
 } else {
   //domainUrl = 'http://bpm.hhtdlng.com';
-  domainUrl = 'http://devbpm.hhtdlng.com'
+  domainUrl = 'http://bpm.hhtdlng.com'
 }
 
 
