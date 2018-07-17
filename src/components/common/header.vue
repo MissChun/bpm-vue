9cc1aa0c9fcfa3ef21498d6f809070fc9ed2d21a
lichunli <lichunli@lichunlideMac-mini.local>
2018-07-17 10:48:23 +0800

    header

---
+++ src/components/common/header.vue
@@ -0,0 +1,31 @@
+<style scoped>
+
+</style>
+<template>
+
+</template>
+<script>
+export default {
+  name: 'publicHeader',
+  props: {},
+
+  data: function() {
+
+    return {
+
+    }
+  },
+  computed: {
+    users: function() {
+      // console.log('users', this.pbFunc.getLocalData('user',true));
+      return this.pbFunc.getLocalData('user', true);
+    }
+
+  },
+  methods: {
+
+  }
+}
+
+</script>
+
