f42385cd38d07106ecda9332ff69bce2bf2f009c
lichunli <lichunli@lichunlideMac-mini.local>
2018-10-28 21:28:45 +0800

    修复bug

--- src/page/basicDataManage/carrierManage/editCarrier.vue
+++ src/page/basicDataManage/carrierManage/editCarrier.vue
@@ -327,7 +327,7 @@ export default {
               });
               if (isReview) {
                 if (this.id) {
-                  this.$router.push({ path: "/basicDataManage/carrierManage/carrierDetail", query: { id: this.id } });
+                  this.$router.push({ path: `/basicDataManage/carrierManage/carrierDetail/${this.id }` });
                 } else {
                   this.$router.push({ path: "/basicDataManage/carrierManage/carrierList" });
                 }
