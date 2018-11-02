a6363d3912a7f03472f486ed58396ebe27361a54
lichunli <lichunli@lichunlideMac-mini.local>
2018-11-02 09:53:25 +0800

    更改测试api链接

--- src/api/index.js
+++ src/api/index.js
@@ -26,7 +26,7 @@ export const getDomainUrl = function(prefix = '') { //掐指一算五个环境
   } else if (currentUrl.match('pbpm.91lng.cn')) { //预发环境
     domainUrl = `${prefix}pbpm.91lng.cn`;
   } else if (currentUrl.match(`bpm.hhtdlng.com`) && !currentUrl.match(`devbpm.hhtdlng.com`)) { //测试环境
-    domainUrl = `${prefix}bpm.hhtdlng.com`;
+    domainUrl = `${prefix}api.hhtdlng.com/bpm`;
   } else if (currentUrl.match(`bpm.91lng.cn`) && !currentUrl.match(`pbpm.91lng.cn`) && !currentUrl.match(`testbpm.91lng.cn`)) {
     //正式环境
     domainUrl = `${prefix}bpm.91lng.cn`;
