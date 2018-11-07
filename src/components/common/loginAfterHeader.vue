a2684ab280c630fc79bfc8054255a14858f43364
lichunli <lichunli@lichunlideMac-mini.local>
2018-11-07 23:16:46 +0800

    小窗口头部修复

--- src/components/common/loginAfterHeader.vue
+++ src/components/common/loginAfterHeader.vue
@@ -49,7 +49,7 @@
   float: left;
   border-bottom: 1px solid rgb(222, 222, 222);
   padding-left:50px;
-  min-width: 800px;
+  min-width: 950px;
 }
 
 .usermenu {
@@ -168,7 +168,16 @@
     bottom: 0;
   }
 }
-
+.users{
+  display:inline-block;
+  max-width:70px;
+  overflow:hidden;
+  text-overflow:ellipsis;
+  white-space:nowrap;
+  line-height: 15px;
+  position:relative;
+  top:3px;
+}
 </style>
 <template>
   <!-- <div style="height: 60px;width: 100;"> -->
@@ -221,7 +230,7 @@
               <span class="ml-25 mr-25 text-stance fs-18">|</span>
               <i class="icon-user"></i>
               <el-dropdown trigger="click" @command="logout">
-                <span class="el-dropdown-link">Hi，{{users.profile.nick_name}}<i class="el-icon-arrow-down el-icon--right"></i></span>
+                <span class="el-dropdown-link">Hi，<span class="users" :title="users.profile.nick_name">{{users.profile.nick_name}}</span><i class="el-icon-arrow-down el-icon--right"></i></span>
                 <el-dropdown-menu slot="dropdown">
                   <el-dropdown-item>退出</el-dropdown-item>
                 </el-dropdown-menu>
