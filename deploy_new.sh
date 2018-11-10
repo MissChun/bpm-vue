c8888826a0af42a9b1e0a05f58dae11820fd5643
LiuMing <601675602@qq.com>
2018-11-10 15:02:03 +0800

    周6bug

--- deploy_new.sh
+++ deploy_new.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "预发发布"
 USER=root
-HOST=39.104.230.233
+HOST=39.104.57.105
 DIR=/www/test/bpm_front
 rm -rf ./dist && npm run build && rsync -avz dist/* ${USER}@${HOST}:${DIR}/
 echo "测试发布成功"
